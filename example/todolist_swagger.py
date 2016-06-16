3fb7d2c9f1e11b62baad3cdc170f3cccbe6b0ea8
Jan Kadlec <jan@localhost.localdomain>
2016-06-16 02:17:03 +0100

    Remove obsolete parameter.

--- example/todolist_swagger.py
+++ example/todolist_swagger.py
@@ -15,4 +15,4 @@
 from restfulswagger import Swagger
 
 swagger = Swagger(info={'version':'0.1', 'description':'TODO list REST API'},
-                  host='localhost:5000', basePath='', schemes=['http'])
+                  host='localhost:5000', schemes=['http'])
